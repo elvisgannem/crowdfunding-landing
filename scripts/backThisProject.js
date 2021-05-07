51ac1b924de5178d159d37c9fde19850789240ba
Elvis Gannem <egannem@gmail.com>
2021-05-07 15:40:38 -0400

    update

--- scripts/backThisProject.js
+++ scripts/backThisProject.js
@@ -3,7 +3,7 @@ function backThisProject() {
     windowBackProject.innerHTML = `
     <div class="thanksWindow">
     
-        <img src="../images/icon-check.svg" alt="Icon Check" class="thanksImg"/>
+        <img src=".././images/icon-check.svg" alt="Icon Check" class="thanksImg"/>
         <h3 class="thanksTitle">Thanks for your support!</h3>
         <p class="thanksParagraph">Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get and email once our campaign is completed.</p>
         <button class="buttons__button hovBtn" onclick="closeThanksWindow()">Got it!</button>
